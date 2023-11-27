dca41ace24321e31db344ff13c0253e3dd76b152
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2023-11-27 23:05:52 +0100

    Add preview

--- fetchReviews.js
+++ fetchReviews.js
@@ -77,3 +77,21 @@ export async function fetchSuperPrawoJazdyReviews(url) {
 	const html = await fetchHTML(url);
 	return await extractSuperProwoJazdyReviews(html);
 }
+
+export async function fetchPreview(url) {
+	// Read the response as text
+	const html = await fetchHTML(url);
+
+	// Create a temporary element to parse the HTML
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(html, 'text/html');
+
+	// Get the title and meta tags from the parsed HTML
+	const title = doc.querySelector('title').innerText;
+	const metaDescription = doc.querySelector('meta[name="description"]');
+	const description = metaDescription ? metaDescription.content : '';
+	const ogImage = doc.querySelector('meta[property="og:image"]');
+	const imageUrl = ogImage ? ogImage.content : '';
+
+	return { title, description, imageUrl };
+}
