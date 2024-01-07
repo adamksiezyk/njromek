84fb5a3d9da1ac3f4c47a2196887ef4ce74e3117
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2024-01-07 19:01:31 +0100

    Refactor header and sidebar to tailwind

--- scripts/animation/sidebar.js
+++ scripts/animation/sidebar.js
@@ -1,17 +1,14 @@
 function toggleSidebar() {
 	var sidebar = document.getElementById('sidebar');
-	var burgerIcon = document.querySelector('.burger-icon');
-	var logoContainer = document.querySelector('.logo-container');
+	var burgerIcon = document.getElementById('burger-icon');
 
 	if (burgerIcon.classList.contains('close')) {
 		sidebar.classList.remove('close');
 		burgerIcon.innerHTML = '&#9776;';
 		burgerIcon.classList.remove('close');
-		logoContainer.classList.remove('close');
 	} else {
 		sidebar.classList.add('close');
 		burgerIcon.innerHTML = '&times;';
 		burgerIcon.classList.add('close');
-		logoContainer.classList.add('close');
 	}
 }
