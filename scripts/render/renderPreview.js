16b5d2b86593d4975f4a92b8a571e38f8f0b2021
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2024-01-27 23:55:56 +0100

    Fix mobile view

--- scripts/render/renderPreview.js
+++ scripts/render/renderPreview.js
@@ -1,30 +1,44 @@
 export function renderPreview(preview, url) {
-	const previewCard = document.createElement('div');
-	previewCard.classList.add('review-card', 'text-gray-900', 'bg-white', 'rounded-lg', 'border', 'border-gray-100', 'shadow', 'dark:border-gray-600', 'w-[768px]', 'p-5', 'dark:bg-gray-800', 'dark:text-white');
+	const previewCard = document.createElement("div");
+	previewCard.classList.add(
+		"review-card",
+		"text-gray-900",
+		"bg-white",
+		"rounded-lg",
+		"border",
+		"border-gray-100",
+		"shadow",
+		"dark:border-gray-600",
+		"w-[350px]",
+		"lg:w-[768px]",
+		"p-5",
+		"dark:bg-gray-800",
+		"dark:text-white",
+	);
 
 	// Create elements to display the preview
 	if (preview.imageUrl) {
-		const imageElement = document.createElement('img');
-		imageElement.classList.add('preview-image');
+		const imageElement = document.createElement("img");
+		imageElement.classList.add("preview-image");
 		imageElement.src = preview.imageUrl;
-		imageElement.alt = 'Preview Image';
+		imageElement.alt = "Preview Image";
 		previewCard.appendChild(imageElement);
 	}
 
-	const titleElement = document.createElement('p');
-	titleElement.classList.add('preview-title');
+	const titleElement = document.createElement("p");
+	titleElement.classList.add("preview-title");
 	titleElement.innerText = preview.title;
 	previewCard.appendChild(titleElement);
 
-	const descriptionElement = document.createElement('p');
-	descriptionElement.classList.add('preview-description');
+	const descriptionElement = document.createElement("p");
+	descriptionElement.classList.add("preview-description");
 	descriptionElement.innerText = preview.description;
 	previewCard.appendChild(descriptionElement);
 
-	const loadMore = document.createElement('a');
-	loadMore.classList.add('preview');
+	const loadMore = document.createElement("a");
+	loadMore.classList.add("preview");
 	loadMore.href = url;
-	loadMore.target = '_blank';
+	loadMore.target = "_blank";
 	loadMore.appendChild(previewCard);
 	return loadMore;
 }
--- scripts/render/renderReview.js
+++ scripts/render/renderReview.js
@@ -1,7 +1,21 @@
 // Function to render reviews
 export function renderSuperPrawoJazdyReview(review) {
-	const reviewCard = document.createElement('div');
-	reviewCard.classList.add('review-card', 'text-gray-900', 'bg-white', 'rounded-lg', 'border', 'border-gray-100', 'shadow', 'dark:border-gray-600', 'w-[768px]', 'p-5', 'dark:bg-gray-800', 'dark:text-white');
+	const reviewCard = document.createElement("div");
+	reviewCard.classList.add(
+		"review-card",
+		"text-gray-900",
+		"bg-white",
+		"rounded-lg",
+		"border",
+		"border-gray-100",
+		"shadow",
+		"dark:border-gray-600",
+		"w-[350px]",
+		"lg:w-[768px]",
+		"p-5",
+		"dark:bg-gray-800",
+		"dark:text-white",
+	);
 
 	// Author, Date, Rating, and Description
 	reviewCard.innerHTML = `
@@ -13,27 +27,27 @@ export function renderSuperPrawoJazdyReview(review) {
 
 	// Pros and Cons
 	if (review.pros.length > 0 || review.cons.length > 0) {
-		const prosConsElement = document.createElement('div');
-		prosConsElement.classList.add('pros-cons');
+		const prosConsElement = document.createElement("div");
+		prosConsElement.classList.add("pros-cons");
 
 		// Pros
 		if (review.pros.length > 0) {
-			const prosElement = document.createElement('div');
-			prosElement.classList.add('pros');
-			prosElement.innerHTML = `<i class="fas fa-thumbs-up"></i> ${review.pros.join(', ')}`;
+			const prosElement = document.createElement("div");
+			prosElement.classList.add("pros");
+			prosElement.innerHTML = `<i class="fas fa-thumbs-up"></i> ${review.pros.join(", ")}`;
 			prosConsElement.appendChild(prosElement);
 		}
 
 		// Cons
 		if (review.cons.length > 0) {
-			const consElement = document.createElement('div');
-			consElement.classList.add('cons');
-			consElement.innerHTML = `<i class="fas fa-thumbs-down"></i> ${review.cons.join(', ')}`;
+			const consElement = document.createElement("div");
+			consElement.classList.add("cons");
+			consElement.innerHTML = `<i class="fas fa-thumbs-down"></i> ${review.cons.join(", ")}`;
 			prosConsElement.appendChild(consElement);
 		}
 
 		reviewCard.appendChild(prosConsElement);
 	}
 
-	return reviewCard
+	return reviewCard;
 }
